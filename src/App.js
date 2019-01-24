cc3ba9e2988aebef586a0eeb881ecf5b75dd7b0d
Shalom Yerushalmy <yershalom@gmail.com>
2019-01-24 12:40:40 +0200

    Put the analytics in index.html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Countdown from './components/Countdown/Countdown';
-import ReactGA from 'react-ga';
 
 class App extends PureComponent {
-  constructor() {
-    super();
-  }
-
-  componentDidMount() {
-    ReactGA.initialize('UA-133173024-1');
-    ReactGA.pageview('/');
-    ReactGA.event({
-      category: 'User',
-      action: 'Page view'
-    });
-  }
 
   render() {
     return (
