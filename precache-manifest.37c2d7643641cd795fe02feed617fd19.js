8a8557e8cfc70634e1c656a45a9501021f6f91ef
Shalom Yerushalmy <yershalom@gmail.com>
2019-01-31 21:59:05 +0200

    Updates

---
+++ precache-manifest.37c2d7643641cd795fe02feed617fd19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a719f64dc4a602f4b0e16539914bcf02",
+    "url": "/gotiscoming/static/media/night.a719f64d.jpg"
+  },
+  {
+    "revision": "5685f78f2703682e46b9",
+    "url": "/gotiscoming/static/js/runtime~main.5685f78f.js"
+  },
+  {
+    "revision": "ea2e121e0a9fe2ccdc89",
+    "url": "/gotiscoming/static/js/main.ea2e121e.chunk.js"
+  },
+  {
+    "revision": "9cb6bfd29a134141d66a",
+    "url": "/gotiscoming/static/js/1.9cb6bfd2.chunk.js"
+  },
+  {
+    "revision": "ea2e121e0a9fe2ccdc89",
+    "url": "/gotiscoming/static/css/main.567cf833.chunk.css"
+  },
+  {
+    "revision": "3ec216a66b5ad345e392d03004f8cd9e",
+    "url": "/gotiscoming/index.html"
+  }
+];
